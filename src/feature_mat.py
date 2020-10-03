a76c42c71d53e887213fa55cd640aba45dab243d
Shashank <43415640+shashankdalmia24@users.noreply.github.com>
2020-10-02 19:00:48 -0700

    Add files via upload

---
+++ src/feature_mat.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+def generate_features():
+    df = pd.read_csv("../data/features.csv")
+    a = df[df.year == 1986].drop(columns = ["agent","is_director","id","year","time"]).to_numpy()
+    b = df[df.year == 1987].drop(columns = ["agent","is_director","id","year","time"]).to_numpy()
+    S = np.stack((a,b))
+    for year in np.arange(1988, 2017):
+        c = df[df.year == year].drop(columns = ["agent","is_director","id","year","time"]).to_numpy()
+        S = np.concatenate((S, [c]))
+    return S
