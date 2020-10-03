e25cd83adb8078a51ca8c1c0c36bc1e041fc95d9
Shashank <43415640+shashankdalmia24@users.noreply.github.com>
2020-10-02 20:52:30 -0700

    changes

--- src/feature_mat.py
+++ src/feature_mat.py
@@ -10,3 +10,7 @@ def generate_features():
         c = df[df.year == year].drop(columns = ["agent","is_director","id","year","time"]).to_numpy()
         S = np.concatenate((S, [c]))
     return S
+
+def get_agents():
+    df = pd.read_csv("../data/features.csv")
+    return df[df.year == 1986].agents.values
